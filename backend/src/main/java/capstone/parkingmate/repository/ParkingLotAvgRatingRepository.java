c136e7c1a7d43fd992057dacd57cc71ec1c0078a
ahffkrmsidQKffl <tksmdch@gmail.com>
2025-05-16 13:31:49 +0900

    Revert "import 누락"
    This reverts commit 647877b7169c1529513954cb8c4f1a1ba02ac6b8.

--- backend/src/main/java/capstone/parkingmate/repository/ParkingLotAvgRatingRepository.java
+++ backend/src/main/java/capstone/parkingmate/repository/ParkingLotAvgRatingRepository.java
@@ -4,7 +4,6 @@ import capstone.parkingmate.entity.ParkingLot;
 import capstone.parkingmate.entity.ParkingLotAvgRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.List;
