23575bb5aea4fb812ca2d20d082401deea8b84cf
ahffkrmsidQKffl <tksmdch@gmail.com>
2025-05-16 13:32:28 +0900

    Revert "같은 곳 이름 규칙 실수"
    This reverts commit 3970375e1dc2f03e2bc812039e209914c5b24091.

--- backend/src/main/java/capstone/parkingmate/repository/ParkingLotAvgRatingRepository.java
+++ backend/src/main/java/capstone/parkingmate/repository/ParkingLotAvgRatingRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import java.util.List;
 
 public interface ParkingLotAvgRatingRepository extends JpaRepository<ParkingLotAvgRating, Long> {
-    Optional<ParkingLotAvgRating> findByParkingLotPId(Long pId);
+    Optional<ParkingLotAvgRating> findByParkingLotPId;
 
 
 }
